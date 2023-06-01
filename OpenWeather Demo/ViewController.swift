80158e1b07a1769f68bf541fe63c78d76a83471f
Tadreik Campbell <tadreik@gmail.com>
2023-06-01 18:58:42 -0400

    Initial Commit

---
+++ OpenWeather Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenWeather Demo
+//
+//  Created by Tadreik Campbell on 6/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
